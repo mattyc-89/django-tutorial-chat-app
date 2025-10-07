4304101db74d5914d873c6599303263f8b4f10a1
mattyc-89 <matthewcoomber89@gmail.com>
2025-10-07 21:14:16 +0100

    v0.1.7: new django key and .env

--- coomberschats/coomberschats/settings.py
+++ coomberschats/coomberschats/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1q--_^zp+8)vyz!zxv0hy+_x&8=ramyqd&bxvqi!-@z*=)d4l5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['coomberchats-cpc2ejbug7fvb5b9.uksouth-01.azurewebsites.net', '127.0.0.1', 'localhost']
 
