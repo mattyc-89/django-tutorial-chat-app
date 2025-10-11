770c61d1cb1d953042abf25297e20beac3d596a9
mattyc-89 <matthewcoomber89@gmail.com>
2025-10-11 19:34:11 +0100

    v0.2.4: try block for storage_backends to fix error 500

--- coomberschats/storage_backends.py
+++ coomberschats/storage_backends.py
@@ -9,14 +9,18 @@ class AzureMediaStorage(AzureStorage):
     container_name = config("AZURE_CONTAINER")
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        account_url = f"https://{self.account_name}.blob.core.windows.net"
-        credential = DefaultAzureCredential()
-        # build a client once and reuse it
-        self._service_client = BlobServiceClient(account_url, credential=credential)
-        self._container_client = self._service_client.get_container_client(
-            self.container_name
-        )
+        try:
+            super().__init__(*args, **kwargs)
+            account_url = f"https://{self.account_name}.blob.core.windows.net"
+            credential = DefaultAzureCredential()
+            # build a client once and reuse it
+            self._service_client = BlobServiceClient(account_url, credential=credential)
+            self._container_client = self._service_client.get_container_client(
+                self.container_name
+            )
+        except Exception as e:
+            print("Error initializing AzureMediaStorage:", e)
+            raise
 
     # override the accessors so django-storages uses our MI-enabled client
     def _get_service_client(self):
