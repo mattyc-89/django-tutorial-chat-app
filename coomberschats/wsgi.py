4329dc3391aac308626296609311f5707ce97c8c
mattyc-89 <matthewcoomber89@gmail.com>
2025-10-12 18:24:46 +0100

    v0.2.6: edited wsgi to get error logs

--- coomberschats/wsgi.py
+++ coomberschats/wsgi.py
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+logger = logging.getLogger(__name__)   # module/class-scoped logger
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coomberschats.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as exc:
+    logger.exception(exc)
